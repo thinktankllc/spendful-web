b7f8dd99eab3cb24230912bacf368b211b282de1
nhan.dev <hey@nhan.dev>
2026-04-05 01:54:21 +0700

    Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
